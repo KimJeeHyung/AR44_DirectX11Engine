349366daa392e736151df30a80ed4c227c2dc708
KimJeeHyung <wlgud1002@gmail.com>
2023-03-27 17:57:45 +0900

    HW3. Noise Shader

--- SHADER_SOURCE/PaintCS.hlsl
+++ SHADER_SOURCE/PaintCS.hlsl
@@ -1,3 +1,27 @@
+float a = 12.9898f;
+float b = 78.233f;
+float c = 43758.5453f;
+float rand(float2 co)
+{
+    return frac(sin(dot(co, float2(a, b))) * c);
+}
+
+uint Hash(uint s)
+{
+    s ^= 2747636419u;
+    s *= 2654435769u;
+    s ^= s >> 16;
+    s *= 2654435769u;
+    s ^= s >> 16;
+    s *= 2654435769u;
+    return s;
+}
+
+float Random(uint seed)
+{
+    return float(Hash(seed)) / 4294967295.0; // 2^32-1
+}
+
 RWTexture2D<float4> tex : register(u0);
 
 // SV_GroupID : 스레드에 속한 그룹의 좌표(Z값)
@@ -13,5 +37,11 @@ void main( uint3 DTid : SV_DispatchThreadID )
         return;
     }
     
-    tex[DTid.xy] = float4(1.f, 0.f, 0.f, 1.f);
+    float w, h;
+    tex.GetDimensions(w, h);
+    float r = Random(DTid.x + DTid.y * w);
+    float g = Random(DTid.x + DTid.y * w);
+    float b = Random(DTid.x + DTid.y * w);
+    
+    tex[DTid.xy] = float4(r, g, b, 1.f);
 }
